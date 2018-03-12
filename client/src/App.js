87258a38106eec14ef6a5c0f79d72affd280e7ab
Mark Apinis <mapinis2019@gmail.com>
2018-03-12 12:12:05 -0400

    React setup and basic layout

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App_text">There are</p>
+        <p className="App_counter">-1</p>
+        <p className="App_text">people here</p>
+      </div>
+    );
+  }
+}
+
+export default App;
